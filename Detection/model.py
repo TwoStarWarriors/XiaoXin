e985af8f08fdb6a15292a6f08b4fcfd3cd9bc635
TwoStarWarriors <1742836029@qq.com>
2025-04-21 11:46:55 +0800

    v2

--- Detection/model.py
+++ Detection/model.py
@@ -10,6 +10,7 @@ import math
 import os
 from scipy.stats import zscore
 import pandas as pd
+import matplotlib.pyplot as plt
 
 file_root_floder="C:\\Users\\86158\\Desktop\\xinnengan\\data"
 out_floder="C:\\Users\\86158\\Desktop\\xinnengan\\mose"
@@ -76,3 +77,51 @@ for file_name in os.listdir(file_root_floder):
         
     df_mose_ac_abs = df_mose.apply(zscore,axis=1).abs()
     df_mose_ac_abs.to_csv(ac_path,index= False,header = False)
+
+#%%开始画图并结束大循环
+fig,axes =plt.subplots(nrows=1,ncols=3,figsize=(63,10))
+# #绘制电压图
+# axes[0].set_title(f"{file_name_mark}_电压信号_极差{range_max_mark}v",fontsize=35)
+# axes[0].set_xlabel('时间',fontsize=25)
+# axes[o].set_ylabel('电压/v',fontsize=25)
+# axes[0].tick_params(axis='both',which='major',labelsize=20)#设置刻度标签的字号
+# for column in window_data.columns:
+#     signal=window_data[column].values
+#     axes[0].plot(time_index, signal, label=column)
+# axes[0].grid()
+#绘制香农熵
+#偏离型度不完整{col_mean_state_mar}
+axes[1].set_title(f"{file_name_mark}_香农构_时间窗{time_windows}_滑动窗口{shannon_windowsize_slide}_偏离型度{col_mean_state_mar}")
+axes[1].set_xlabel('时间',fontsize=25)
+axes[1].set_ylabel('香农熵',fontsize=25)
+axes[1].tick_params(axis='both',which='major',labelsize=20)# 设置刻度标签的字号
+df_mose_index =df_mose.index
+for column in df_mose.columns:
+    signal=df_mose[column].values
+    axes[1].plot(df_mose_index, signal, label=column)
+axes[1].grid()
+#绘制异常系数
+#编离程度不完整{col mean state m}
+axes[2].set_title(f"{file_name_mark}_异常系数_时间窗口{time_windows}_滑动窗{shannon_windowsize_slide}_编离程度{col_mean_state_m}")
+axes[2].set_xlabel('时间',fontsize=25)
+axes[2].set_ylabel('异常系数Ac',fontsize=25)
+axes[2].tick_params(axis='both',which='major',labelsize=20)#设置刻度标签的字号
+df_mose_ac_abs_nonan_index=df_mose_ac_abs_nonan.index
+for column in df_mose_ac_abs_nonan.columns:
+    signal=df_mose_ac_abs_nonan[column].values
+    axes[2].plot(df_mose_ac_abs_nonan_index, signal, label=column)
+axes[2].grid()
+fig.suptitle(f"{file_name_mark}_{start_index}_{end_index}",fontsize=55)
+handles, labels =axes[0].get_legend_handles_labels()
+fig.legend(handles, labels, loc='upper right',bbox_to_anchor=(1,0.92), fontsize=25)
+#调整子图之间的间距
+plt.tight_layout()
+#创建个图例
+plt.subplots_adjust(right=0.92)
+fig_file_name = f"{file_name_mark}_汇总图_{start_index}.png"
+out_path =os.path.join(plots_floder, fig_file_name)
+plt.savefig(out_path, dpi=100)
+plt.show()
+plt.close()
+start_index += window_size_move
+end_index += window_size_move
