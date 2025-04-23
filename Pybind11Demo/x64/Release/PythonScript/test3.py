0aa80b259ceb3b1f672bd57cff64ee9a9e63af34
TwoStarWarriors <1742836029@qq.com>
2025-04-23 10:14:15 +0800

    v0

---
+++ Pybind11Demo/x64/Release/PythonScript/test3.py
@@ -0,0 +1,10 @@
+import sys
+
+def Add ( a ):
+    return 5
+
+    #调用sum函数
+
+def StrTest ( str ):
+    print(str)
+    return "asdf"
---
+++ Pybind11Demo/x64/Release/PythonScriptPathConf.py
@@ -0,0 +1,5 @@
+import sys
+import pathlib
+import os
+  
+sys.path.append(os.path.join(pathlib.Path(__file__).parent.absolute(), 'PythonScript'))
